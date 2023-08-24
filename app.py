3af2c3298d579a3a394f23df12f9ae5d8cb70dc3
apricot496 <apricut496flobs@gmail.com>
2023-08-24 20:59:13 +0900

    最初のコミット

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import oauth2client
+
+# ページのタイトルとアイコンを設定
+st.set_page_config(page_title="test2")
+st.write('ポチャ')
